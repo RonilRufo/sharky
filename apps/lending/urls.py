80c8eab89641673d7fd47a1875394e5d29d14ebd
Ronil Rufo <ronil.rufo@gmail.com>
2021-05-16 11:52:02 +0800

    Implemented loan sources graph in dashboard page

--- apps/lending/urls.py
+++ apps/lending/urls.py
@@ -3,9 +3,10 @@ URL patterns in lending app
 """
 from django.urls import path
 
-from .views import EarningsGraph
+from .views import EarningsGraph, LoanSourcesGraph
 
 app_name = "lending"
 urlpatterns = [
     path("earnings-graph/", EarningsGraph.as_view(), name="earnings-graph"),
+    path("loan-sources-graph/", LoanSourcesGraph.as_view(), name="loan-sources-graph"),
 ]
--- apps/lending/views.py
+++ apps/lending/views.py
@@ -1,11 +1,12 @@
 from dateutil.relativedelta import relativedelta
 
+from django.db.models import Count, Q
 from django.http import Http404
 from django.http.response import JsonResponse
 from django.utils import timezone
 from django.views.generic import View
 
-from apps.lending.models import Amortization, Loan
+from apps.lending.models import Amortization, CapitalSource, Loan
 
 
 class EarningsGraph(View):
@@ -35,6 +36,46 @@ class EarningsGraph(View):
             graph_data.append(Loan.objects.filter(id__in=loan_ids).total_interest_earned())
 
             loan_date += relativedelta(months=1)
+
+        return JsonResponse(
+            {
+                "labels": labels,
+                "graph_data": graph_data,
+            }
+        )
+
+
+class LoanSourcesGraph(View):
+    """
+    Returns data for plotting the loan sources graph in dashboard page.
+    """
+
+    def get(self, request, *args, **kwargs):
+        """
+        Handles the GET request to this view. Only accepts aJax requests.
+        """
+        is_ajax = request.META.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest"
+        if not is_ajax:
+            raise Http404()
+
+        labels = [source[1] for source in CapitalSource.SOURCES]
+        data = Loan.objects.aggregate(
+            savings=Count(
+                "pk",
+                filter=Q(source__capital_source__source=CapitalSource.SOURCES.savings)
+            ),
+            credit_card=Count(
+                "pk",
+                filter=Q(
+                    source__capital_source__source=CapitalSource.SOURCES.credit_card
+                )
+            ),
+            cash_loan=Count(
+                "pk",
+                filter=Q(source__capital_source__source=CapitalSource.SOURCES.loan)
+            ),
+        )
+        graph_data = [value for key, value in data.items()]
 
         return JsonResponse(
             {
