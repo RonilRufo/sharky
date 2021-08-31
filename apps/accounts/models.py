0e1ce91dd41bf9f254ca5f866935dd916562fcf6
Ronil Rufo <ronil.rufo@gmail.com>
2021-08-31 10:17:51 +0800

    Added is_capital_source_provider field into EmailUser model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -80,6 +80,10 @@ class EmailUser(AbstractBaseUser, PermissionsMixin, UUIDPrimaryKeyMixin):
         default=False,
         help_text=_("If the user is a borrower or not."),
     )
+    is_capital_source_provider = models.BooleanField(
+        default=False,
+        help_text=_("If the user provides a capital source or not."),
+    )
 
     USERNAME_FIELD = "email"
     EMAIL_FIELD = "email"
